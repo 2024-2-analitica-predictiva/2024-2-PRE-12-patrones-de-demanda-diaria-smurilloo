c3f9e44c95394326d39d7c8c591010eabcf5e6bd
smurilloo <143961450+smurilloo@users.noreply.github.com>
2024-12-07 09:38:42 -0500

    Update test_homework.py

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -7,17 +7,17 @@ def test_homework():
     """Test the homework."""
 
     assert os.path.exists(
-        "files/data/demanda-comercial-dias.csv"
-    ), "files/data/demanda-comercial-dias.csv file is missing"
+        "../files/data/demanda-comercial-dias.csv"
+    ), "../files/data/demanda-comercial-dias.csv file is missing"
 
     assert os.path.exists(
-        "files/plots/demanda-comercial-patrones-ejemplo.png"
-    ), "files/plots/demanda-comercial-patrones-ejemplo.png file is missing"
+        "../files/plots/demanda-comercial-patrones-ejemplo.png"
+    ), "../files/plots/demanda-comercial-patrones-ejemplo.png file is missing"
 
     assert os.path.exists(
-        "files/plots/demanda-comercial-perfiles.png"
-    ), "files/plots/demanda-comercial-perfiles.png file is missing"
+        "../files/plots/demanda-comercial-perfiles.png"
+    ), "../files/plots/demanda-comercial-perfiles.png file is missing"
 
     assert os.path.exists(
-        "files/plots/demanda-comercial.png"
-    ), "files/plots/demanda-comercial.png file is missing"
+        "../files/plots/demanda-comercial.png"
+    ), "../files/plots/demanda-comercial.png file is missing"
